2749e05a1e3f554a3dff1ffe0a9d3d7771f43902
Gamepro5 <gamepro543@gmail.com>
2020-07-17 09:38:39 -0700

    Near perfect fix by "hints"

--- entities/player/player_controller.gd
+++ entities/player/player_controller.gd
@@ -24,10 +24,10 @@ export(int) var sprint_speed = 16
 export(int) var acceleration = 8
 export(int) var deacceleration = 10
 export(float, 0.0, 1.0, 0.05) var air_control = 0.3
-export(int) var jump_height = 10
+export(int) var jump_height = 5
 # Slopes
 export(float) var floor_max_angle = 45.0
-var gravity_velocity := Vector3()
+
 ##################################################
 
 # Called when the node enters the scene tree
@@ -43,10 +43,16 @@ func _process(_delta: float) -> void:
 	
 	camera_rotation()
 
-
+#TODO: stop deceleration when on a wall, fix not going up and down slopes at the same speed, also see comment on line 133.
+var wall_counter = 0
+var air_counter = 0
+var floor_counter = 0
+var curr_gravity = 0
+var _temp_vel
+var on_floor = false
+var floor_normal = Vector3()
 # Called every physics tick. 'delta' is constant
 func _physics_process(delta: float) -> void:
-	
 	# Input
 	direction = Vector3()
 	var aim: Basis = get_global_transform().basis
@@ -60,22 +66,20 @@ func _physics_process(delta: float) -> void:
 		direction += aim.x
 	direction.y = 0
 	direction = direction.normalized()
-	
+
 	# Jump
 	var _snap: Vector3
-	#if (moving): #idk how to stop on slopes now
-		#gravity_velocity.y = 0
-	if is_on_floor():
-		gravity_velocity.y = -0.0001
-		_snap = Vector3(0, -1, 0)
+	if on_floor:
+		_snap = Vector3(0, -0.1, 0)
+		velocity.y = 0
+		#curr_gravity = 0
 		if Input.is_action_just_pressed("move_jump"):
 			_snap = Vector3(0, 0, 0)
-			gravity_velocity.y = jump_height
-	else:
-		# Apply Gravity
-		velocity.y = 0;
-		gravity_velocity.y -= gravity * delta
+			velocity.y = jump_height
 	
+	# Apply Gravity
+	curr_gravity -= gravity * delta
+
 	# Sprint
 	var _speed: int
 	if (Input.is_action_pressed("move_sprint") and can_sprint and move_axis.x >= 0.5):
@@ -87,6 +91,7 @@ func _physics_process(delta: float) -> void:
 		cam.set_fov(lerp(cam.fov, FOV, delta * 8))
 		sprinting = false
 	
+	
 	# Acceleration and Deacceleration
 	# where would the player go
 	var _temp_vel: Vector3 = velocity
@@ -97,7 +102,7 @@ func _physics_process(delta: float) -> void:
 		_temp_accel = acceleration
 	else:
 		_temp_accel = deacceleration
-	if not is_on_floor():
+	if not on_floor:
 		_temp_accel *= air_control
 	# interpolation
 	_temp_vel = _temp_vel.linear_interpolate(_target, _temp_accel * delta)
@@ -111,28 +116,57 @@ func _physics_process(delta: float) -> void:
 		if velocity.z < _vel_clamp and velocity.z > -_vel_clamp:
 			velocity.z = 0
 	"""
-	var temp_vel = velocity;
-	temp_vel.y = 0
-	temp_vel = direction * _speed;
-	velocity.x = temp_vel.x;
-	velocity.z = temp_vel.z;
+	_temp_vel = direction * _speed
 	"""
-	
-	# new idea: have a ray pointing in the direction you are moving on your feet. if it finds somethign that is steeper than the 
-	
 	# Move
-	#move_and_slide(gravity_velocity, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle))
-	if (is_on_floor()):
+	print(velocity)
+	if on_floor:
+		# set gravity to 0 or jump speed
+		curr_gravity = velocity.y
+		# slide input vector surface normal
+		_temp_vel = _temp_vel.slide(floor_normal)
+		# reapply gravity, so jumps go up rather than normal to the floor
+		_temp_vel.y += curr_gravity
 		if  (is_on_wall()):
-			velocity = move_and_slide(velocity, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle))
+			wall_counter += 1
+			#print("wall: ", wall_counter)
+			#setting velocity to the remainder solves weird behavior on super steep slopes but fucks things up on perfect 90 degree walls when on a slope. idk how to have it both ways.
+			velocity = move_and_slide(_temp_vel, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle))
 		else:
-			velocity.y = move_and_slide_with_snap(velocity, _snap, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle)).y
-		#velocity = move_and_slide_with_snap(velocity, _snap, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle))
+			move_and_slide(_temp_vel, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle), false)
+
+		# manually control this since we don't always have downward pressure above. could use a raycast
+		on_floor = false
+		floor_normal = null
+
+		# snap if vector exists
+		if _snap != Vector3():
+			# collision check
+			var col = move_and_collide(_snap, false, false, true)
+			# if no collision, we're not on the floor
+			if col:
+				if acos(col.normal.dot(FLOOR_NORMAL)) < deg2rad(floor_max_angle) + 0.01:
+					var travel = col.travel.project(FLOOR_NORMAL)
+					# move object if it's super far away, otherwise just chill
+					if abs(travel.length()) > 0.1:
+						global_transform.origin += travel
+					# we're now on the floor
+					on_floor = true
+					floor_normal = col.normal
+
+			# print(col.normal.dot(FLOOR_NORMAL) < deg2rad(floor_max_angle),  col.travel.project(FLOOR_NORMAL))
 	else:
-		velocity = move_and_slide(velocity, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle))
-	move_and_slide(gravity_velocity, FLOOR_NORMAL, false, 4, deg2rad(floor_max_angle))	
-	print(velocity);
-	
+		air_counter += 1
+		#print("air:  ", air_counter, " ", on_floor)
+		_temp_vel.y = velocity.y
+		_temp_vel.y += curr_gravity
+		if (is_on_ceiling()):
+			curr_gravity = 0;
+			velocity.y = 0;
+		move_and_slide(_temp_vel, FLOOR_NORMAL, true, 4, deg2rad(floor_max_angle))
+		on_floor = is_on_floor()
+		floor_normal = get_floor_normal()
+
 
 
 # Called when there is an input event
